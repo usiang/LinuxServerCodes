1c0b8959440b190314c955d0fcca94e77cb4b151
Buge <ssisoo@live.cn>
2019-11-13 15:33:00 +0800

    添加阅读提示

--- 6/6-3testsendfile.cpp
+++ 6/6-3testsendfile.cpp
@@ -12,51 +12,69 @@
 #include <fcntl.h>
 #include <sys/sendfile.h>
 
-int main( int argc, char* argv[] )
+int main(int argc, char *argv[])
 {
-    if( argc <= 3 )
+    if (argc <= 3)
     {
-        printf( "usage: %s ip_address port_number filename\n", basename( argv[0] ) );
+        printf("usage: %s ip_address port_number filename\n", basename(argv[0]));
         return 1;
     }
-    const char* ip = argv[1];
-    int port = atoi( argv[2] );
-    const char* file_name = argv[3];
+    // 接受IP地址
+    const char *ip = argv[1];
 
-    int filefd = open( file_name, O_RDONLY );
-    assert( filefd > 0 );
+    // 接受端口号
+    int port = atoi(argv[2]);
+
+    // 接受文件地址
+    const char *file_name = argv[3];
+
+    // 打开文件，获取文件句柄
+    int filefd = open(file_name, O_RDONLY);
+    assert(filefd > 0);
+
+    // 通过文件描述符获取文件状态
     struct stat stat_buf;
-    fstat( filefd, &stat_buf );
+    fstat(filefd, &stat_buf);
 
+    // 初始化IP地址结构体
     struct sockaddr_in address;
-    bzero( &address, sizeof( address ) );
+    bzero(&address, sizeof(address));
+
+    // 设置协议族
     address.sin_family = AF_INET;
-    inet_pton( AF_INET, ip, &address.sin_addr );
-    address.sin_port = htons( port );
+    // 将IP地址字符串表示转化为网络字节序整数表示
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    // 赋值端口
+    address.sin_port = htons(port);
 
-    int sock = socket( PF_INET, SOCK_STREAM, 0 );
-    assert( sock >= 0 );
+    // 创建socket
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
 
-    int ret = bind( sock, ( struct sockaddr* )&address, sizeof( address ) );
-    assert( ret != -1 );
+    // 命名绑定socket到指定的地址
+    int ret = bind(sock, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != -1);
 
-    ret = listen( sock, 5 );
-    assert( ret != -1 );
+    // 开启监听并设置半连接队列为5
+    ret = listen(sock, 5);
+    assert(ret != -1);
 
+    // 接受来自客户端的连接，并接受地址
     struct sockaddr_in client;
-    socklen_t client_addrlength = sizeof( client );
-    int connfd = accept( sock, ( struct sockaddr* )&client, &client_addrlength );
-    if ( connfd < 0 )
+    socklen_t client_addrlength = sizeof(client);
+    int connfd = accept(sock, (struct sockaddr *)&client, &client_addrlength);
+
+    if (connfd < 0)
     {
-        printf( "errno is: %d\n", errno );
+        printf("errno is: %d\n", errno);
     }
     else
     {
-        sendfile( connfd, filefd, NULL, stat_buf.st_size );
-        close( connfd );
+        // 发送文件
+        sendfile(connfd, filefd, NULL, stat_buf.st_size);
+        close(connfd);
     }
 
-    close( sock );
+    close(sock);
     return 0;
 }
-
